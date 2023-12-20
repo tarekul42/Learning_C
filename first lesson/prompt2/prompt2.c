75dbd6e9efd9c52ca1128a21e7e9502f42713ff6
tarekul42 <tarekulrifat205@gmail.com>
2023-12-21 00:50:16 +0600

    take 2 number and print them

---
+++ first lesson/prompt2/prompt2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+    int number1, number2;
+    printf("Please enter your first number: ");
+    scanf("%d", &number1);
+    printf("Please enter your second number: ");
+    scanf("%d", &number2);
+    printf("Numbers are: %d, %d", number1, number2);
+    return 0;
+}
