03fb01bc7eb46d8815bb2bf654187269cde3c69d
tarekul42 <tarekulrifat205@gmail.com>
2023-12-20 20:06:11 +0600

    take value from user / scanf learn

---
+++ first lesson/prompt/prompt.c
@@ -0,0 +1,12 @@
+/*
+TODO: write a program that takes an intiger and print that number
+*/
+
+#include<stdio.h>
+
+int main(){
+    int num;
+    printf("Please enter a number: ");
+    scanf("%d", &num);
+    printf("You have pressed: %d\n", num);
+}
