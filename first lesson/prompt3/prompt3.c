d5056c354a94a345d80bdc45bb6d40605919f80a
tarekul42 <tarekulrifat205@gmail.com>
2023-12-21 15:03:31 +0600

    take 2 value using a function

---
+++ first lesson/prompt3/prompt3.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main() {
+    int num1, num2;
+    printf("Enter your numbers: ");
+    scanf("%d %d", &num1, &num2);
+
+    printf("Your numbers are %d, %d", num1, num2);
+    return 0;
+}
