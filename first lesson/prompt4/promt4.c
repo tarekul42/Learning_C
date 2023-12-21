b0ae8e1b36861c0aee864dfae0740c895258485d
tarekul42 <tarekulrifat205@gmail.com>
2023-12-21 15:08:51 +0600

    take 2 float number from user and print it.

---
+++ first lesson/prompt4/promt4.c
@@ -0,0 +1,10 @@
+// take float number from user and print it.
+
+#include<stdio.h>
+int main(){
+    float num1, num2;
+    printf("Enter your floating numbers: ");
+    scanf("%f %f", &num1, &num2 );
+
+    printf("Your floating numbers are: %.1f, %.1f", num1, num2);
+}
